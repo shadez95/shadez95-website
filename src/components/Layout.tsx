f3359c8da6e208aef83d4043b2ce11546a928aa7
edgar begay <dixonbegay@gmail.com>
2019-06-11 19:23:02 -0700

    Cleaned up web manifest options and configuration

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -30,13 +30,6 @@ export const Layout: React.FC = ({ children }): JSX.Element => (
           <title>{data.site.siteMetadata.title}</title>
           <meta name="description" content={data.site.siteMetadata.description} />
 
-          <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-icon.png" />
-          <link rel="icon" type="image/png" href="/assets/favicon-16x16.png" sizes="16x16" />
-          <link rel="icon" type="image/png" href="/assets/favicon-32x32.png" sizes="32x32" />
-          <link rel="icon" type="image/png" href="/assets/favicon-96x96.png" sizes="96x96" />
-
-          <meta name="theme-color" content="#162b35" />
-
           <meta property="og:type" content="website" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="https://shadez95.dev" />
